ee7689630beb7e46338d4270aaa1d93acb79778b
Anthony Shellman <atshellman@gmail.com>
2023-12-22 13:56:06 -0700

    Add and configure Accordian component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,23 @@
-
+import Accordian from './Accordian'
+import data from './../data/data'
 
 const Card = () => {
+
+  const accordians = data.map(item => {
+    const {id, question, answer} = item
+    return (
+      <Accordian
+        key={id}
+        question={question}
+        answer={answer}
+      />
+    )
+  })
 
   return (
     <>
     <section>
-      <h1>FAQs</h1>
+      { accordians }
     </section>
   </>
   )
