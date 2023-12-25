0e06f74b8bc944c5395dd40778a2e862be0c71eb
Anthony Shellman <atshellman@gmail.com>
2023-12-24 19:01:41 -0700

    Add section and class

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Card from './components/Card'
 import './styles/_global.scss'
+import './styles/app.scss'
 import mobileHeaderImage from './assets/images/background-pattern-mobile.svg'
 
 const App = () => {
   return (
     <>
-      <header>
-        <img src={mobileHeaderImage} alt="Mobile Header Image" />
-      </header>
-      <Card />
+      <section className='card-and-header-section'>
+        <header>
+          <img src={mobileHeaderImage} alt="Mobile Header Image" />
+        </header>
+        <Card />
+      </section>
     </>
   )
 }
