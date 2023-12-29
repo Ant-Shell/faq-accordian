a3551e54e49870f3d8d4a5b69b2dcfc1d319f620
Anthony Shellman <atshellman@gmail.com>
2023-12-28 17:13:00 -0700

    Remove commented code

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import mobileHeaderImage from './assets/images/background-pattern-mobile.svg'
 const App = () => {
   return (
     <>
-      {/* <section className='card-container'> */}
         <header>
           <img className='header-background' src={mobileHeaderImage} alt="Mobile Header Image" />
         </header>
           <Card />
-      {/* </section> */}
     </>
   )
 }
